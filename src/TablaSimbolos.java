5ab968d6a365e2dba8109eb4ce7b137730b525b4
haide <haideg.93@gmail.com>
2018-04-16 19:55:34 -0400

    Tabla de simbolos

---
+++ src/TablaSimbolos.java
@@ -0,0 +1,32 @@
+/**
+ * Created by Haide on 03/04/2018.
+ */
+public class TablaSimbolos {
+    public final Integer L_CORCHETE = 73;
+    public final Integer R_CORCHETE = 74;
+    public final Integer L_LLAVE = 75;
+    public final Integer R_LLAVE = 76;
+    public final Integer COMA = 77;
+    public final Integer DOS_PUNTOS = 78;
+    public final Integer LITERAL_CADENA = 79;
+    public final Integer PR_TRUE = 80;
+    public final Integer PR_FALSE = 81;
+    public final Integer PR_NULL = 82;
+    public final Integer EOF = 83;
+
+    class Entrada{
+        int compLex;
+
+    }
+
+    private static void insertTablaSimbolos(String componente, Character caracter){
+
+    }
+
+    public static void inicializarTablaSimbolos(){
+        insertTablaSimbolos(",",',');
+    }
+
+
+}
+
