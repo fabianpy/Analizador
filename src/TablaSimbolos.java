adaf4e5c7b10502b8423d31c7321c1bb3f25816f
haide <haideg.93@gmail.com>
2018-04-18 19:39:01 -0400

    Se agrego insertarTabla a  tabla de simbolos

--- src/TablaSimbolos.java
+++ src/TablaSimbolos.java
@@ -4,39 +4,37 @@ import java.util.HashMap;
  * Created by Haide on 03/04/2018.
  */
 public class TablaSimbolos {
-    public final Integer L_CORCHETE = 73;
-    public final Integer R_CORCHETE = 74;
-    public final Integer L_LLAVE = 75;
-    public final Integer R_LLAVE = 76;
-    public final Integer COMA = 77;
-    public final Integer DOS_PUNTOS = 78;
-    public final Integer LITERAL_CADENA = 79;
-    public final Integer PR_TRUE = 80;
-    public final Integer PR_FALSE = 81;
-    public final Integer PR_NULL = 82;
-    public final Integer EOF = 83;
+    public static final Integer L_CORCHETE = 73;
+    public static final Integer R_CORCHETE = 74;
+    public static final Integer L_LLAVE = 75;
+    public static final Integer R_LLAVE = 76;
+    public static final Integer COMA = 77;
+    public static final Integer DOS_PUNTOS = 78;
+    public static final Integer LITERAL_CADENA = 79;
+    public static final Integer PR_TRUE = 80;
+    public static final Integer PR_FALSE = 81;
+    public static final Integer PR_NULL = 82;
+    public static final Integer EOF = 83;
 
     class Entrada{
         int compLex;
     }
 
-//    private static void insertTablaSimbolos(String componente, Integer caracter){
-//        HashMap tabla = new HashMap();
-//        tabla.put()
-//    }
-//
-//    public static void inicializarTablaSimbolos(){
-//        insertTablaSimbolos("{",'{');
-//        insertTablaSimbolos("}",'}');
-//        insertTablaSimbolos("[",'[');
-//        insertTablaSimbolos("]",']');
-//        insertTablaSimbolos(",",',');
-//        insertTablaSimbolos(":",':');
-//        insertTablaSimbolos(";",';');
-//        insertTablaSimbolos("(",'(');
-//        insertTablaSimbolos(")",')');
-//
-//    }
+    private static void insertTablaSimbolos(String componente, Integer caracter){
+        HashMap tabla = new HashMap();
+        tabla.put(componente, caracter);
+    }
+
+    public static void inicializarTablaSimbolos(){
+        insertTablaSimbolos("{", L_LLAVE);
+        insertTablaSimbolos("}", R_LLAVE);
+        insertTablaSimbolos("[", L_CORCHETE);
+        insertTablaSimbolos("]", R_CORCHETE);
+        insertTablaSimbolos(",", COMA);
+        insertTablaSimbolos(":", DOS_PUNTOS);
+        insertTablaSimbolos("True", PR_TRUE);
+        insertTablaSimbolos("False", PR_FALSE);
+    }
 
 
 }
