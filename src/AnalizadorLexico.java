3f5ce4538a66176182ee783a118da6ac92038672
fabianpy <naibaf.martinez@gmail.com>
2018-04-22 23:36:53 -0400

    ok 1

--- src/AnalizadorLexico.java
+++ src/AnalizadorLexico.java
@@ -1,6 +1,4 @@
 
-import javafx.scene.control.Tab;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -26,36 +24,28 @@ public class AnalizadorLexico {
         HashMap<String, Object> hm = new HashMap<>();
         String id = Character.toString(c).toLowerCase();
         try {
-            System.out.println("valor de c en analizar palabra reservada = " + c);
 
             while (array[j+1] != '"' && array[j+1] != ' ' && array[j+1] != ',') {
                 if (c == '\n')
                     error("Se llegó al final de la línea sin finalizar el nombre del identificador.");
-                System.out.println("   VALOR ACTUAL DE ID = "+id);
                 j=j+1;
                 c = array[j];
                 id += Character.toLowerCase(c);
-                System.out.println("valor de c en analizar palabra reservada 2 = " + c);
             }
 
         } catch (ArrayIndexOutOfBoundsException e) {
             error("Se llegó al final del archivo sin finalizar el nombre del identificador. Se esperaba: '\"' ");
             e.printStackTrace();
         }
-        System.out.println("valor de id a retornar = " + id);
         hm.put("palabra", id);
         hm.put("indice", j);
-        System.out.println("valor de id a retornar despues = " + id);
         return hm;
     }
 
     private static void generarOutput(ArrayList<String> valueList){
-        System.out.println("entra en generar output");
-
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(DIRECTORIO_ACTUAL.concat("output.txt")))) {
 
             for (String value : valueList) {
-                System.out.println("value: " + value); //borrar
                 bw.write(value + " ");
             }
 
@@ -72,8 +62,6 @@ public class AnalizadorLexico {
         while (i < array.length) {
             char c = array[i];
 
-            System.out.println("linea "+ nroLinea +" ; valor de c = '" + c + "' ; valor de i = "+i);
-
             if (c == ' ' || c == '\t') {
                 i += 1;
                 contenidoList.add(String.valueOf(c));
@@ -82,7 +70,6 @@ public class AnalizadorLexico {
 
             else if (c == '\n') { //si es salto de línea, aumenta la cantidad
                 nroLinea += 1;
-                System.out.println("ES UN SALTO DE LINEA. linea = "+ nroLinea); //borrar
                 i += 1;
                 contenidoList.add(String.valueOf(c));
                 continue;
@@ -113,11 +100,10 @@ public class AnalizadorLexico {
                             error("Se llegó al final de la línea sin finalizar el nombre del identificador. Se esperaba: '\"' ");
                         id = id + c;
                     } while (c != '"' && c != '\n');
-                    System.out.println("  AGREGA ID A LA TABLA. id = " + id); //borrar
                     if (TablaSimbolos.buscar(id) == null) {
                         TablaSimbolos.insertTablaSimbolos(id, null);
-                        contenidoList.add("LITERAL_CADENA");
                     }
+                    contenidoList.add("LITERAL_CADENA");
 
                 } catch (ArrayIndexOutOfBoundsException e) {
                     error("Se llegó al final del archivo sin finalizar el nombre del identificador. Se esperaba: '\"' ");
@@ -138,14 +124,12 @@ public class AnalizadorLexico {
                                 case 0:
                                     //es un entero
                                     numero += c; //arma el numero
-                                    if (array[i+1] == ',' || array[i+1] == '\n')
-                                        acepta = true;
+                                    if (array[i+1] == ',' || array[i+1] == '\n' || array[i+1] == ' ')
+                                        estado = 3;
                                     else if (array[i+1] == '.')
                                         estado = 1;
                                     else if (Character.toLowerCase(array[i+1]) == 'e')
                                         estado = 2;
-                                    else if (Character.toLowerCase(array[i+1]) == ' ')
-                                        estado = 3;
                                     break;
                                 case 1:
                                     //es un punto decimal
@@ -171,9 +155,10 @@ public class AnalizadorLexico {
                                     break;
                                 case 3:
                                     //número podría ser válido
-                                    if (c == ' ') {
+                                    if (c == ' ' || c == '\n' || c == ',') {
                                         contenidoList.add("LITERAL_NUM");
                                         acepta = true;
+                                        i = i-2;
                                     } else {
                                         throw new Exception("Caso no contemplado. c = " + c);
                                     }
@@ -185,22 +170,11 @@ public class AnalizadorLexico {
                                     acepta = true; //para que salga del ciclo
                                     break;
                                 default:
-                                    /*System.out.println("estado en default = " + estado);
-                                    acepta = true;*/
                                     throw new Exception("Caso no contemplado: '"+c+"'");
                             }
-                            System.out.println("   VALOR ACTUAL DE numero = "+numero);
-                        } /*else if (acepta) {
-                            //TODO: agregar 'id' a la tabla de símbolos
-                            contenidoList.add("LITERAL_NUM");
-
-                        }*/ /*else {
-                        error("'"+numero+"' no es nu número válido.");
-                        acepta = false;
-                    }*/
+                        }
                         i+=1;
                     }
-
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -212,49 +186,36 @@ public class AnalizadorLexico {
             } else if (Character.toLowerCase(c) == 't') {
                 //PR_TRUE
                 //puede formarse la palabra 'true'
-                System.out.println("puede formarse la palabra 'true'");
                 HashMap<String, Object> map = analizarPalabraReservada(array, i);
-                System.out.println("palabra formada: " + map.get("palabra"));
                 if (map.get("palabra").equals("true")) {
                     contenidoList.add("PR_TRUE");
                 } else {
                     error("No se reconoce '"+map.get("palabra")+"' como componente del lenguaje.");
                 }
                 i = (Integer) map.get("indice");
-                System.out.println(" valor de i = " + i);
             } else if (Character.toLowerCase(c) == 'f') {
                 //PR_FALSE
                 //puede formarse la palabra 'false'
-                System.out.println("puede formarse la palabra 'false'");
                 HashMap<String, Object> map = analizarPalabraReservada(array, i);
-                System.out.println("palabra formada: " + map.get("palabra"));
                 if (map.get("palabra").equals("false")) {
-                    System.out.println("valor de palabra : " + map.get("palabra"));
                     contenidoList.add("PR_FALSE");
                 } else {
                     error("No se reconoce '"+map.get("palabra")+"' como componente del lenguaje.");
                 }
                 i = (Integer) map.get("indice");
-                System.out.println(" valor de i = " + i);
             } else if (Character.toLowerCase(c) == 'n') {
                 //PR_NULL
                 //puede formarse la palabra 'null'
-                System.out.println("puede formarse la palabra 'null'");
                 HashMap<String, Object> map = analizarPalabraReservada(array, i);
-                System.out.println("palabra formada: " + map.get("palabra"));
                 if (map.get("palabra").equals("null")) {
                     contenidoList.add("PR_NULL");
                 } else {
                     error("No se reconoce '"+map.get("palabra")+"' como componente del lenguaje.");
                 }
                 i = (Integer) map.get("indice");
-                System.out.println(" valor de i = " + i);
-            } /*else if (c == 'E') {
-                //EOF
-            }*/
+            }
 
             i += 1;
-            //contenidoList.add("LITERAL_CADENA"); //TODO: cambiar por los valores del key
 
         }
         generarOutput(contenidoList);
@@ -265,16 +226,14 @@ public class AnalizadorLexico {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese la ruta hasta el archivo: ");
-        // String path = scanner.nextLine();
-        String path = "C:\\fp\\compiladores\\Analizador\\archivos\\prueba.txt"; //ubicación del archivo de prueba. Cambiar después por Scanner
+        String path = scanner.nextLine();
 
         try {
             //lee el archivo según el path que fue introducido
             String contenido = new String(Files.readAllBytes(Paths.get(path)));
-            //System.out.println(contenido); //borrar
             TablaSimbolos.inicializarTablaSimbolos();
             siguienteLexema(contenido);
-            TablaSimbolos.tabla.forEach((k, v) -> System.out.println("Key: " + k + ": Value: " + v));
+            //TablaSimbolos.tabla.forEach((k, v) -> System.out.println("Key: " + k + ": Value: " + v));
         } catch (IOException e) {
             e.printStackTrace();
         }
